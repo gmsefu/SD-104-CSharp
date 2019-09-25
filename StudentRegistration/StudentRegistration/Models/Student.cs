be9d2586932bc0a3f8d5ed6308c7d1d9af477eae
Max Mwak <guybarack@hotmail.com>
2019-09-25 09:35:52 -0500

    hands on Student registration

---
+++ StudentRegistration/StudentRegistration/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentRegistration.Models
+{
+    public class Student
+    {
+        [Display(Name = "ID")]
+        public int StudentID { get; set; }
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+        [Display(Name = "Cell Phone Number")]
+        public string StudentCellPhone { get; set; }
+        [Display(Name = "Age")]
+        public int StudentAge { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string StudentEmail { get; set; }
+
+
+    }
+}
